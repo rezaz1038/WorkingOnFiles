86383ebe5e6c5a817cb9fda8ce47558106cbba60
reza1038 <rezaz1038@gmail.com>
2021-01-30 00:29:17 +0330

    first

--- WorkingOnFiles/Program.cs
+++ WorkingOnFiles/Program.cs
@@ -1,4 +1,5 @@
-﻿    using System;
+﻿using System;
+using System.IO;
 
 namespace WorkingOnFiles
 {
@@ -6,6 +7,28 @@ namespace WorkingOnFiles
     {
         static void Main(string[] args)
         {
+
+
+            //folder create 
+
+            System.IO.Directory.CreateDirectory(@"d:\tset\d2");
+            Console.WriteLine((System.IO.Directory.Exists(@"d:\tset\d2")) ? "Folder exist" : "folder not exist");
+
+            try
+            {
+             if (File.Exists(@"d:\tset\d2\re123.txt")) 
+             System.IO.File.Move(@"d:\tset\d2\re123.txt", @"d:\tset\d2\123.txt");
+            }
+            catch (Exception  )
+            {
+
+                throw;
+            }
+            //file rename
+
+
+
+
             Console.WriteLine("Hello World!");
         }
     }

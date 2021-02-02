c5bed69cc7967cc47bbab04805c4329fa69ec98f
reza1038 <rezaz1038@gmail.com>
2021-02-02 06:34:33 +0330

    secend edition

--- WorkingOnFiles/Program.cs
+++ WorkingOnFiles/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
+using System.Xml;
 
 namespace WorkingOnFiles
 {
@@ -9,6 +11,48 @@ namespace WorkingOnFiles
         {
 
 
+            //////read multiple xml
+
+            var path = @"d:\dirXml\";
+            var resultfiles = System.IO.Directory.GetFiles(path, "*.xml", SearchOption.AllDirectories);
+
+
+            foreach (var file in resultfiles)
+            {
+                ProcessFile(file);
+            }
+
+            /////////
+            Console.WriteLine("Hello World!");
+        }
+
+        static void ProcessFile(string Filename)
+        {
+
+            int totalC = 0;
+
+            XmlDocument xmlDoc = new XmlDocument();
+
+            xmlDoc.Load(Filename);
+
+            XmlNodeList Clist = xmlDoc.GetElementsByTagName("ID");
+            totalC = Clist.Count;
+            Console.WriteLine(Filename);
+            for (int i = 0; i < Clist.Count; i++)
+            {
+                Console.WriteLine(Clist[i].InnerText.ToString());
+                System.Threading.Thread.Sleep(100);
+            }
+
+            Console.WriteLine(Environment.NewLine);
+
+
+        }
+
+        static void ProcessingFile()
+        {
+            #region file
+
             //folder create 
 
             System.IO.Directory.CreateDirectory(@"d:\tset\d2");
@@ -16,20 +60,19 @@ namespace WorkingOnFiles
 
             try
             {
-             if (File.Exists(@"d:\tset\d2\re123.txt")) 
-             System.IO.File.Move(@"d:\tset\d2\re123.txt", @"d:\tset\d2\123.txt");
+                if (File.Exists(@"d:\tset\d2\re123.txt"))
+                    System.IO.File.Move(@"d:\tset\d2\re123.txt", @"d:\tset\d2\123.txt");
             }
-            catch (Exception  )
+            catch (Exception)
             {
 
                 throw;
             }
-            //file rename
+            ////file rename
+
+            #endregion
 
 
-
-
-            Console.WriteLine("Hello World!");
         }
     }
 }
